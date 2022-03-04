09d18306be1def667d65badc4b3f9bc948315b58
lanceljr <lanceljr98@gmail.com>
2022-03-04 15:40:23 +0800

    user microservice done

--- user/user_api.js
+++ user/user_api.js
@@ -6,26 +6,6 @@ const db = require("./user")
 const firestore = db.db
 
 
-
-
-
-
-// //2nd api: get game by id 
-// router.get("/getGame", async (req,res) => {
-//     let gameID='500'
-//     try{
-//         let game = await firestore.collection('Games').doc(gameID).get()
-//         res.send(game.data().gameTitle)
-
-//     }
-//     catch(error){
-//         res.send('fuck u')
-//     }
-
-// })
-
-
-
 //1. get all user
 router.get("/getAllUser", async (req, res) => {
     let all_users=[]
@@ -39,40 +19,50 @@ router.get("/getAllUser", async (req, res) => {
 
 //2. get a user
 router.get("/getUser", async (req,res)=> {
-    let userID = req.query['openid.identity'].split("/")[5]
-    try{
-        let user = await firestore.collection('Users').doc(userID).get()
-        if(user.exists){
-            res.send(userID)
-        }
-        else{
-            res.send("User does not exist")
-        }
+    // let userID = req.query['openid.identity'].split("/")[5]
+    let userID='12354697'
+    let user = await firestore.collection('Users').doc(userID).get()
+
+    let data={
+        email:'',
+        tradeID:''
     }
-    catch(error){
-        res.send('fuck u')
+
+    if(!user.exists){
+        await firestore.collection("Users").doc(userID).set(data);
+        res.send("New User has been created")
+    }
+    else{
+        res.send(userID)
     }
 
 })
 
-//3. create user
-router.post("/createUser", async (req,res)=>{
-    //1.  get email and trade URL from user 
+//3. update user
+router.post("/updateUser", async (req,res)=>{
+
+
+    //1.  get email, tradeID, steamID from user 
     // let email = req.body.email
-    // let trade_url=req.body.tradeURL
-
-
-    // let userID = req.query['openid.identity'].split("/")[5]
+    // let tradeID=req.body.tradeURL
+    // let steamID = req.body.
+    let userID='12354697'
 
     let email = 'hehe@gmail.com'
-    let trade_url='www.facebook.com';
+    let tradeID='45678';
     data={
         email: email,
-        tradeURL: trade_url
+        tradeID: tradeID
     }
-    // firestore.collection('Users').doc(email).set(new_user)
 
+    try{
+        await firestore.collection('Users').doc(userID).set(data)
+        res.send("User has been updated!")
 
+    }
+    catch{
+        res.send("Error")
+    }
 })
 
 

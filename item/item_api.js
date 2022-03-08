5e5af695abde48a91c7a5f1bf5b8740459dfc048
Liye Ng <liye1998@hotmail.com>
2022-03-08 17:16:17 +0800

    Item Microservice
    Added Item Microservice, 1 API created (getAllItems)

---
+++ item/item_api.js
@@ -0,0 +1,40 @@
+
+const { MongoClient } = require("mongodb");
+const { mainModule } = require("process");
+
+
+const express = require('express');
+// const res = require('express/lib/response');
+const router = express.Router();
+
+const uri = "mongodb+srv://admin:4azF5gfOoIR8hQ3P@itemdb.rwiet.mongodb.net/itemDB?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, async(err, database) => {
+    if (err) return console.log(err)
+    db = database.db('itemDB');
+    console.log("Item microservice is now online")
+    
+    
+});
+
+
+// 1st API: GET all items (from both CSGO and Dota 2)
+router.get("/getAllItems", async (req,res) => {
+    all_items=[];
+    let csitems = await db.collection('csgoitems').find().toArray()
+    let dotaitems = await db.collection('dotaitems').find().toArray()
+    csitems.forEach(doc=>{
+        all_items.push(doc)
+    })
+    dotaitems.forEach(doc=>{
+        all_items.push(doc)
+    })
+    res.send(all_items);
+})
+
+// 2nd API: GET specific item 
+
+
+
+module.exports = router;

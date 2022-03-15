9d3ad6647f1108f337539063e0c7aa079f36ca3a
randall yeo <randall.yeo@isobar.com>
2022-03-15 22:41:29 +0800

    modified trade microservice to use graphql

--- trade/trade.js
+++ trade/trade.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors')
+const {graphqlHTTP} = require('express-graphql');
+const {buildSchema} = require('graphql')
+
 const mongoose = require("mongoose");
 const Trade = require("./model/trade")
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const trade = require('./model/trade');
 
 const app = express();
 var jsonParser = bodyParser.json()
@@ -11,53 +15,122 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+
+
 app.use((req, res, next)=>{
     console.log(`${req.method} - ${req.url}`)
     next()
 })
 
-app.get("/trade", async (req, res)=>{
-    
-    try {
-        const trades = await Trade.find();
-        console.log(trades)
-        res.status(200).json({trades})
-
-    } catch (err) {
-        console.log(err);
-    }
-})
-
-app.get("/trade/items", async (req, res)=>{
-    const items = req.query.items.split("_");
-    try {
-        var selectedTrades = []
-        const trades = await Trade.find();
-        for (trade of trades){
-            var offer = trade.offerItems;
-            var receive = trade.receieveItems;
-            var status = true;
-            for (item of items){
-                if (!offer.includes(item) || !offer.includes(item)){
-                    status = false;
-                    break
-                }
-                
-            }
-            if (status){
-                selectedTrades.push(trade)
-            }
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type rootQuery {
+            trades: [Trades]
+            
         }
 
-        res.status(200).json({trades: selectedTrades})
+        type mutationQuery {
+            createTrade(trade: Trade): Boolean
+            tradeItems(items: [Int]): [Trades]
+        }
+
+        input Trade {
+            receiveItems: [Int]
+            offerItems: [Int]
+            userId: Int
+        }
+
+        type Trades {
+            _id: String
+            offerItems: [Int]
+            userId: Int
+            receiveItems: [Int]
+            status: Boolean
+            
+        }
+
+        schema {
+            query: rootQuery
+            mutation: mutationQuery
+        }
+    `),
+    rootValue: {
+        trades: async ()=>{
+            try {
+                
+                var trades = await Trade.find();
+                
+                var new_trades = []
+                for (var trade of trades){
+                    
+                    new_trades.push({_id: trade._id, userId: trade.userId, offerItems: trade.offerItems, receiveItems: trade.receiveItems, status: trade.status})
+                }
+                console.log(new_trades)
+                return new_trades
+                
+                
         
+            } catch (err) {
+                console.log(err);
+                return "Failed";
+            }
+            
+        },
+        tradeItems: async (args)=>{
+            const {items} = args;
+            try {
+                var selectedTrades = []
+                const trades = await Trade.find();
+                for (var trade of trades){
+                    
+                    var offer = trade.offerItems;
+                    var receive = trade.receiveItems;
+                    console.log(receive)
+                    var status = false;
+                    
+                    for (let item of items){
+                        
+                        if (offer.includes(item) || receive.includes(item)){
+                            status = true;
+                            break
+                        }
+                        
+                    }
+                    if (status){
+                        selectedTrades.push({_id: trade._id, userId: trade.userId, offerItems: trade.offerItems, receiveItems: trade.receiveItems, status: trade.status})
+                    }
+                }
+                console.log(selectedTrades)
+                return selectedTrades
+                
+        
+            } catch (err) {
+                console.log(err);
+            }
+        },
+        createTrade: async (args)=>{
+            console.log(args)
+            const {receiveItems, offerItems, userId} = args.trade;
+            console.log(userId)
+            try {
+                
+                const trades = await Trade.insertMany({ userId, receiveItems, offerItems, "status": false })
+                return true
+                
 
-    } catch (err) {
-        console.log(err);
-    }
-})  
+            } catch (err) {
+                
+                console.log(err);
+                return false
+            }
+        }
+    },
+    graphiql: true
+}));
 
-app.get("/trade/users", async (req, res)=>{
+
+
+/* app.get("/trade/users", async (req, res)=>{
     const userId = req.query.id;
     try {
         
@@ -69,24 +142,11 @@ app.get("/trade/users", async (req, res)=>{
     } catch (err) {
         console.log(err);
     }
-})
+}) */
 
-app.post("/trade/new_trade", async (req, res)=>{
-    const {receiveItems, offerItems, userId} = req.body;
-    try {
-        
-        const trades = await Trade.insertMany({ userId, receiveItems, offerItems, "status": false })
-        res.status(201).json({message: "Success"})
-        
 
-    } catch (err) {
-        res.status(400).json({message: "Failed"})
-        console.log(err);
-    }
-    
-})
 
-app.post("/trade/update", async (req, res)=>{
+/* app.post("/trade/update", async (req, res)=>{
     const {tradeID} = req.body;
     try {
         
@@ -100,7 +160,7 @@ app.post("/trade/update", async (req, res)=>{
     } catch (err) {
         console.log(err);
     }
-})
+}) */
 
 
 

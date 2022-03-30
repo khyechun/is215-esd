3fe69a49fc0124a40985d903b846c9e0f2b23f52
randall yeo <randall.yeo@isobar.com>
2022-03-30 22:33:20 +0800

    added headers for complex services

--- getItems/getItems.js
+++ getItems/getItems.js
@@ -20,7 +20,9 @@ app.use(bodyParser.json())
 
 app.use((req, res, next)=>{
     console.log(`${req.method} - ${req.url}`)
-    
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
     next()
 })
 

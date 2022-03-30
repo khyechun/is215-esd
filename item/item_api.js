500ed88f0bc5f32ac0c88ffb1052899c16b9e9ca
Liye Ng <liye1998@hotmail.com>
2022-03-30 18:43:03 +0800

    Update to item_api
    Made changes to output of API calls to send status code + result

--- item/item_api.js
+++ item/item_api.js
@@ -10,100 +10,150 @@ const router = express.Router();
 const uri = "mongodb+srv://admin:4azF5gfOoIR8hQ3P@itemdb.rwiet.mongodb.net/itemDB?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 
-MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, async(err, database) => {
+MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, async (err, database) => {
     if (err) return console.log(err)
     db = database.db('itemDB');
     console.log("Item microservice is now online")
-    
-    
+
+
 });
 
 
 // 1st API: GET all items (from both CSGO and Dota 2)
-router.get("/getAllItems", async (req,res) => {
-    var gameId = req.query.gameId
-    if (gameId == 570){
-        let dotaitems = await db.collection('dotaitems').find().toArray()
-        res.send(dotaitems)
+router.get("/getAllItems", async (req, res) => {
+    if (res.statusCode == 200) {
+        var gameId = req.query.gameId
+        if (gameId == 570) {
+            let dotaitems = await db.collection('dotaitems').find().toArray()
+            res.send({
+                "code": res.statusCode,
+                "items": dotaitems
+            }
+            )
+        } else {
+            let csitems = await db.collection('csgoitems').find().toArray()
+            res.send({
+                "code": res.statusCode,
+                "items": csitems
+            }
+            )
+        }
     } else {
-        let csitems = await db.collection('csgoitems').find().toArray()
-        res.send(csitems)
+        res.send(
+            {
+                "code": res.statusCode,
+                "message": `An error: ${res.statusCode} occurred while trying to retrieve the items. Please try again.`
+            })
+
     }
-   /*  all_items=[];
-    let csitems = await db.collection('csgoitems').find().toArray()
-    let dotaitems = await db.collection('dotaitems').find().toArray()
-    
-    csitems.forEach(doc=>{
-        all_items.push(doc)
-    })
-    dotaitems.forEach(doc=>{
-        all_items.push(doc)
-    })
-    res.send(all_items); */
+    /*  all_items=[];
+     let csitems = await db.collection('csgoitems').find().toArray()
+     let dotaitems = await db.collection('dotaitems').find().toArray()
+     
+     csitems.forEach(doc=>{
+         all_items.push(doc)
+     })
+     dotaitems.forEach(doc=>{
+         all_items.push(doc)
+     })
+     res.send(all_items); */
 })
 
 // 2nd API: GET specific item 
 
-router.get("/getItem/:itemID", async (req,res) => {
-    var id = req.params.itemID;
-    console.log(id)
+router.get("/getItem/:itemID", async (req, res) => {
+    if (res.statusCode == 200) {
+        var id = req.params.itemID;
+        console.log(id)
 
-    db.collection('csgoitems').findOne({itemID: id}, function(err, result) {
-        console.log(result);
-        if(result == null) {
-            db.collection('dotaitems').findOne({itemID: id}, function(err, result) {
-                res.send(result)
+        db.collection('csgoitems').findOne({ itemID: id }, function (err, result) {
+            console.log(result);
+            if (result == null) {
+                db.collection('dotaitems').findOne({ itemID: id }, function (err, result) {
+                    res.send(
+                    {
+                        "code": res.statusCode,
+                        "item": result
+                    }
+
+                    )
+                })
+            } else {
+                res.send(
+                    {
+                        "code": res.statusCode,
+                        "item": result
+                    }
+                )
+            }
+        })
+    } else { 
+        res.send(
+            {
+                "code": res.statusCode,
+                "message": `An error: ${res.statusCode} occurred while trying to retrieve the item. Please try again.`
             })
-        } else {
-            res.send(result)
-        }
-    })
+    }
 })
 
 
 // 3rd API: GET specific item(s)
 
-router.get("/getItems", async (req,res) => {
+router.get("/getItems", async (req, res) => {
+    if (res.statusCode == 200) {
     var ids = req.query.arr;
     var ids = ids.split('|');
     var result = [];
-    for (i=0; i < ids.length; i++) {
+    for (i = 0; i < ids.length; i++) {
         var items = ids[i];
         var id_arr = items.split(",")
         var offer = id_arr[0]
         var receive = id_arr[1]
         var offer_arr = []
         var receive_arr = []
-        for (id of offer.split(".")){
-            db.collection('csgoitems').findOne({itemID: id}, function(err, result) {
+        for (id of offer.split(".")) {
+            db.collection('csgoitems').findOne({ itemID: id }, function (err, result) {
                 console.log(result);
-                if(result == null) {
-                    db.collection('dotaitems').findOne({itemID: id}, function(err, result) {
-                        offer_arr.push({itemID: result.itemID, itemName: result.itemName, icon_url: result.icon_url, rarity_colour: result.rarity_colour});
+                if (result == null) {
+                    db.collection('dotaitems').findOne({ itemID: id }, function (err, result) {
+                        offer_arr.push({ itemID: result.itemID, itemName: result.itemName, icon_url: result.icon_url, rarity_colour: result.rarity_colour });
                     })
                 } else {
-                    offer_arr.push({itemID: result.itemID, itemName: result.itemName, icon_url: result.icon_url, rarity_colour: result.rarity_colour});
+                    offer_arr.push({ itemID: result.itemID, itemName: result.itemName, icon_url: result.icon_url, rarity_colour: result.rarity_colour });
                 }
             })
         }
 
-        for (id of receive.split(".")){
-            db.collection('csgoitems').findOne({itemID: id}, function(err, result) {
+        for (id of receive.split(".")) {
+            db.collection('csgoitems').findOne({ itemID: id }, function (err, result) {
                 console.log(result);
-                if(result == null) {
-                    db.collection('dotaitems').findOne({itemID: id}, function(err, result) {
-                        receive_arr.push({itemID: result.itemID, itemName: result.itemName, icon_url: result.icon_url, rarity_colour: result.rarity_colour});
+                if (result == null) {
+                    db.collection('dotaitems').findOne({ itemID: id }, function (err, result) {
+                        receive_arr.push({ itemID: result.itemID, itemName: result.itemName, icon_url: result.icon_url, rarity_colour: result.rarity_colour });
                     })
                 } else {
-                    receive_arr.push({itemID: result.itemID, itemName: result.itemName, icon_url: result.icon_url, rarity_colour: result.rarity_colour});
+                    receive_arr.push({ itemID: result.itemID, itemName: result.itemName, icon_url: result.icon_url, rarity_colour: result.rarity_colour });
                 }
             })
         }
-        result.push({offer: offer_arr, receive: receive_arr})
-        
+        result.push({ offer: offer_arr, receive: receive_arr })
+
     }
-    res.send(result);
-})
+    res.send(
+        {
+            "code": res.statusCode,
+            "items": result
+        }
+        );
+} else {
+    res.send(
+        {
+            "code": res.statusCode,
+            "message": `An error: ${res.statusCode} occurred while trying to retrieve the items. Please try again.`
+        }
+    )
+}}
+)
 
 
 module.exports = router;

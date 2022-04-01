6f5f872ca02885dc1787545ac8483680286ced8e
randall yeo <randall.yeo@isobar.com>
2022-04-01 20:18:42 +0800

    updated complex services

--- listTrade/listTrade.js
+++ listTrade/listTrade.js
@@ -1,121 +1,66 @@
-const express = require('express');
-const cors = require('cors')
+const express = require("express");
 const axios = require('axios')
+const cors = require("cors");
+const connect_amqp = require("../rabbitMQ_AMQP/rabbitMQ_AMQP_Setup")
 
-const { Kafka } = require("kafkajs");
-const {graphqlHTTP} = require('express-graphql');
-const {buildSchema} = require('graphql')
-const {errorName} = require('./constants')
-const {errorType} = require('./constants')
-const getErrorCode = errorName =>{
-    return errorType[errorName]
-}
-const kafka = require("./kafka")
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const app = express();
-var jsonParser = bodyParser.json()
-app.use(cors())
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next)=>{
     console.log(`${req.method} - ${req.url}`)
     
-
     next()
 })
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+const tradeURL = 'http://localhost:8084/api/trade/createTrade'
 
-const tradeURL = 'http://localhost:8085/api/trade';
-const authenticateURL = 'http://localhost:8082/api/authenticate_api/authenticateToken'
+app.post("/api/list_trade", async (req, res) => {
+    /* const {receiveItems, offerItems} = req.body; */
+    const {receiveItems, offerItems, token} = req.body;
+    console.log(req.body)
+    try {
+        var res = await axios.get(authenticateURL, setHeader(token));
+        var steamId = res.userId
+        
+        /* var steamId = 76561198000003391 */
+        var data = JSON.stringify({query: `mutation{
+            createTrade(trade: {receiveItems: [${receiveItems.join(", ")}],
+          offerItems: [${offerItems.join(", ")}], steamId: ${steamId}}) 
+          }`})
+        console.log(data)
+        var status = await axios.post(tradeURL, data, setHeader());
+        
+        
+        // AMQP THINGS: TO DO
+        /* const activity = await kafka.produceActivity(`${steamId} has placed a trade offer.`) */
+        /* var data = {email: "HEHHEHE", tradeID: status.data._id}
+        await amqp_function.connect("email", data) */
+        res.statusCode = 201
+        res.status(201).send({status:true})
+        
+        
 
-app.use('/api/list_trade', (req,res)=>{
-    graphqlHTTP({
-        schema: buildSchema(`
-            type rootQuery {
-                getTrades(items: [Int]): Boolean
-            }
-    
-            type mutationQuery {
-                createTrade(trade: Trade): Boolean
-            }
-
-            input Trade {
-                receiveItems: [Float]
-                offerItems: [Float]
-                token: String
-            }
-
-            schema {
-                query: rootQuery
-                mutation: mutationQuery
-            }
-        `),
-        rootValue: {
-            createTrade: async (args)=>{
-                
-                const {receiveItems, offerItems} = args.trade;
-                console.log(receiveItems)
-                console.log(offerItems)
-                try {
-                    var res = await axios.get(authenticateURL, setHeader(token));
-                    var steamId = res.userId
-                    
-                    var steamId = 76561198000003391
-                    var data = JSON.stringify({query: `query{
-                        trades {
-                            _id: String
-                            offerItems
-                            steamId
-                            receiveItems
-                            status
-                        }
-                      }`})
-                    console.log(data)
-                    var status = await axios.post(tradeURL, data, setHeader());
-                    console.log(status)
-                    /* const activity = await kafka.produceActivity(`${steamId} has placed a trade offer.`) */
-                    
-                    res.statusCode = 201
-                    res.json({status:true})
-                    
-                    
-            
-                } catch (err) {
-                    /* const activity = await kafka.produceError(`ERROR`) */
-                    console.log(err)
-                    console.log(errorName.CREATETRADE)
-                    throw new Error(errorName.CREATETRADE)
-                    
-                }
-                
-            },
-            
-        },
-        graphiql: true,
-        customFormatErrorFn: (err)=>{
-            
-            const error = getErrorCode(err.message);
-            
-            try {
-                res.status(500).send({ message: "WRONG"})
-            }catch(err){
-                res.status(500).send({ message: error.message, statusCode: error.statusCode})
-            }
-            
-        }
-    })(req,res)
+    } catch (err) {
+        /* const activity = await kafka.produceError(`ERROR`) */
+        console.log(err)
+        
+        
+    }
 });
 
-const setHeader = (token)=>{
+const setHeader = ()=>{
     return {
       headers:{
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer '+ token
+                'Content-Type': 'application/json'
               }
     }
 }
 
-app.listen(process.env.PORT || 8092, console.log("Running this app on 8092"))
+
+app.listen(process.env.PORT || 8092, console.log("Running this app on 8092"));

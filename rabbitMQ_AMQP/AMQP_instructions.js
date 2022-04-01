b977da30819e91dc07523236a61d2fa13c8c61d0
Caleb Shawn <caleb.ng.2020@scis.smu.edu.sg>
2022-04-01 18:07:09 +0800

    PUSH

---
+++ rabbitMQ_AMQP/AMQP_instructions.js
@@ -0,0 +1,27 @@
+// Docker Image for rabbitMQ = docker run --name rabbitmq -p 15672:15672 rabbitmq:3-management
+
+
+const amqp_function = require("./rabbitMQ_AMQP_Setup")
+
+var data = {
+    "name" : "Caleb"
+}
+
+async function testing2(){
+    //await amqp_function.connect("error")
+    //await amqp_function.connect("activity")
+    await amqp_function.connect("email")
+
+    
+    //await amqp_function.sendData("error", data)
+   // await amqp_function.sendData("activity", data)
+   await amqp_function.sendData("email", data)
+
+
+
+
+
+}
+
+
+testing2()

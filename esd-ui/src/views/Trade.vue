8556d234ddb5be1481414400d5b79d229778fe37
liew khyechun <khyechun@gmail.com>
2022-04-02 11:09:12 +0800

    trade search popover UI

--- esd-ui/src/views/Trade.vue
+++ esd-ui/src/views/Trade.vue
@@ -2,14 +2,15 @@
   <div class="container-fluid" align="left">
     <h3 align="center " class="fw-bold mt-4">Trades</h3>
     <div class="row" v-for="(trade, index) of trades" :key="index">
-      <div class="col-5">
-        <p>Lance offers...</p>
+      <div class="col-5 mt-2">
+        <p class="mb-1">Lance offers...</p>
         <div class="trade-container">
           <div class="y-scroll-overflow">
             <div
               v-for="(item, index_1) of trade.offerItems"
               :key="index_1"
               class="item-container"
+              v-bind:style="{ borderBottomColor: '#' + item.rarity_colour }"
             >
               <img
                 style="width: 100%"
@@ -52,6 +53,7 @@
             v-for="(item, index_1) of trade.receiveItems"
             :key="index_1"
             class="item-container"
+            v-bind:style="{ borderBottomColor: '#' + item.rarity_colour }"
           >
             <img
               style="width: 100%"
@@ -71,19 +73,201 @@
       </div>
     </div>
   </div>
+  <div class="accordion accordion-flush" id="accordionFlushExample">
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="flush-headingOne">
+        <button
+          style="
+            position: absolute;
+            bottom: 0;
+            background-color: #595cb9;
+            color: white;
+          "
+          class="accordion-button collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#flush-collapseOne"
+          aria-expanded="false"
+          aria-controls="flush-collapseOne"
+        >
+          Open Trade Search Menu
+        </button>
+      </h2>
+      <div
+        id="flush-collapseOne"
+        class="accordion-collapse collapse"
+        aria-labelledby="flush-headingOne"
+        data-bs-parent="#accordionFlushExample"
+      >
+        <div
+          class="accordion-body"
+          style="
+            position: absolute;
+            bottom: 52px;
+            height: 380px;
+            background-color: white;
+            width: 100%;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+          "
+        >
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-6">
+                <div class="row mb-4">
+                  <div class="col-8">
+                    <input
+                      v-model="search"
+                      style="
+                        border-radius: 10px;
+                        border: 0;
+                        height: 35px;
+                        background-color: #c4c4c4;
+                      "
+                      placeholder="Search ..."
+                      class="form-control w-100"
+                    />
+                  </div>
+                  <div class="col-3" align="right">
+                    <select class="form-select custom-select">
+                      <option value="csgo" selected>CSGO</option>
+                      <option value="dota">DOTA</option>
+                    </select>
+                  </div>
+                  <div class="col-1">
+                    <button class="btn">
+                      <img
+                        style="width: 25px"
+                        src="../assets/images/logo/icons8-refresh-60.png"
+                      />
+                    </button>
+                  </div>
+                </div>
+                <div class="row item-scroll" align="left" v-if="!loading">
+                  <div
+                    class="col-3 mb-3"
+                    v-for="item of filteredItems"
+                    :key="item._id"
+                  >
+                    <div
+                      class="item-container"
+                      v-bind:style="{
+                        borderBottomColor: '#' + item.rarity_colour,
+                      }"
+                      @click="selectItems(item)"
+                    >
+                      <img
+                        style="width: 100%"
+                        :src="
+                          'https://steamcommunity-a.akamaihd.net/economy/image/' +
+                          item.img_url
+                        "
+                      />
+                      <div
+                        class="p-1 pb-0"
+                        style="font-size: 12px"
+                        v-html="item.itemName"
+                      ></div>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="d-flex align-items-center justify-content-center"
+                  style="min-height: 300px"
+                  v-else
+                >
+                  <Loader />
+                </div>
+              </div>
+              <div class="col-6">
+                <div
+                  class="background-secondary"
+                  align="left"
+                  style="
+                    border-radius: 20px;
+                    padding: 10px;
+                    color: white;
+                    height: 310px;
+                  "
+                >
+                  <h6>Selected Items</h6>
+                  <div
+                    v-if="selectedItems.length == 0"
+                    class="mt-4"
+                    align="center"
+                  >
+                    <p class="mb-0">Your Searched Items</p>
+                    <small style="color: #ffffff5f"
+                      >Add items you want to search for</small
+                    >
+                  </div>
+                  <div class="row item-scroll">
+                    <div
+                      class="col-3 mb-3"
+                      v-for="(item, index) of selectedItems"
+                      :key="item._id"
+                    >
+                      <div
+                        class="item-container"
+                        style="position: relative"
+                        v-bind:style="{
+                          borderBottomColor: '#' + item.rarity_colour,
+                        }"
+                      >
+                        <button
+                          class="btn btn-danger remove-item-btn"
+                          style="color: white"
+                          @click="removeItem(index)"
+                        >
+                          <img
+                            style="width: 10px"
+                            src="../assets/images/logo/icons8-close-48.png"
+                          />
+                        </button>
+                        <img
+                          style="width: 100%"
+                          :src="
+                            'https://steamcommunity-a.akamaihd.net/economy/image/' +
+                            item.img_url
+                          "
+                        />
+                        <div
+                          class="p-1 pb-0"
+                          style="font-size: 12px"
+                          v-html="item.itemName"
+                        ></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div align="right"><button class="btn btn-1">Search</button></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 
 <script>
 const axios = require("axios").default;
+import Loader from "@/components/Loader.vue";
+const api = require("../api");
 
 export default {
   name: "Trade",
   components: {
     // HelloWorld,
+    Loader,
   },
   data() {
     return {
+      loading: false,
+      items: [],
+      selectedItems: [],
+      search: "",
       trades: [
         {
           _id: "",
@@ -170,10 +354,36 @@ export default {
           // always executed
         });
     },
+    selectItems(item) {
+      if (!this.selectedItems.map((i) => i.itemID).includes(item.itemID)) {
+        this.selectedItems.push(item);
+      }
+    },
+    removeItem: function (index) {
+      this.selectedItems.splice(index, 1);
+    },
   },
-  mounted: function () {
+  mounted: async function () {
     console.log("hi");
     this.redirect();
+
+    const itemResponse = await api.getItems(730);
+    this.items = itemResponse.slice(0, 100);
+  },
+  computed: {
+    filteredItems: function () {
+      if (this.search == "") {
+        return this.items;
+      }
+      let filteredArr = [];
+      this.items.map((item) => {
+        if (item.itemName.toLowerCase().includes(this.search.toLowerCase())) {
+          filteredArr.push(item);
+        }
+      });
+      // console.log(filteredArr);
+      return filteredArr;
+    },
   },
 };
 </script>
@@ -182,7 +392,9 @@ export default {
 .trade-container {
   border-radius: 15px;
   padding: 10px;
-  background-color: #595cb9a9;
+  /* background-color: #595cb9a9; */
+  /* background-color: #c4c4c4; */
+  background-color: rgba(109, 146, 202, 0.55);
 }
 
 .item-container {
@@ -192,10 +404,12 @@ export default {
   border-bottom-width: 8px;
   border-bottom-style: solid;
   transition: 0.2s;
-  width: 150px;
+  width: 120px;
   display: inline-block;
   margin: 10px;
   white-space: normal;
+  color: white;
+  cursor: pointer;
 }
 
 .item-container:hover {
@@ -229,5 +443,18 @@ export default {
 /* Handle on hover */
 ::-webkit-scrollbar-thumb:hover {
   background: #555;
+}
+
+.item-scroll {
+  max-height: 240px;
+  overflow-y: scroll;
+}
+
+.remove-item-btn {
+  border-radius: 5px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  padding: 0 7px;
 }
 </style>
